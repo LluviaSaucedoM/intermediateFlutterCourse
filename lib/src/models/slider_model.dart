243adb541ae4811dea4b9912db9690d181604596
Lluvia Saucedo <lluvia.saucedo.socialty@gmail.com>
2022-07-22 12:31:49 -0500

    SliderModel and adding provider

---
+++ lib/src/models/slider_model.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SliderModel with ChangeNotifier {
+  late double _currentPage = 0;
+  // ignore: unnecessary_getters_setters
+  double get currentPage => _currentPage;
+
+  set currentPage(double currentPage) {
+    _currentPage = currentPage;
+    notifyListeners();
+  }
+}
--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -8,8 +8,102 @@ class SlideShowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: SvgPicture.asset('assets/svgs/slide-1'),
-    ));
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Expanded(child: _Slides()),
+            _Dots(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Dots extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 70,
+      color: Colors.blue[200],
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const <Widget>[
+          _Dot(index: 0),
+          _Dot(index: 1),
+          _Dot(index: 2),
+        ],
+      ),
+    );
+  }
+}
+
+class _Dot extends StatelessWidget {
+  const _Dot({
+    Key? key,
+    required this.index,
+  }) : super(key: key);
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 15,
+      height: 15,
+      margin: const EdgeInsets.symmetric(horizontal: 7),
+      decoration:
+          BoxDecoration(color: Colors.grey[300], shape: BoxShape.circle),
+    );
+  }
+}
+
+class _Slides extends StatefulWidget {
+  @override
+  State<_Slides> createState() => _SlidesState();
+}
+
+class _SlidesState extends State<_Slides> {
+  final pageViewController = PageController();
+
+  @override
+  void initState() {
+    pageViewController.addListener(() {
+      print('pagina numero:${pageViewController.page}');
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    pageViewController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(controller: pageViewController, children: const <Widget>[
+      _Slid(svg: 'assets/svgs/slide-1.svg'),
+      _Slid(svg: 'assets/svgs/slide-2.svg'),
+      _Slid(svg: 'assets/svgs/slide-3.svg'),
+    ]);
+  }
+}
+
+class _Slid extends StatelessWidget {
+  const _Slid({
+    Key? key,
+    required this.svg,
+  }) : super(key: key);
+  final String svg;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      padding: const EdgeInsets.all(30),
+      child: SvgPicture.asset(svg),
+    );
   }
 }
